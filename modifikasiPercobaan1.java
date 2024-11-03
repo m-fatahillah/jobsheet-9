3c04b599becedb25df171e8430d8f542e617ee5f
m-fatahillah <fatahcrown789@gmail.com>
2024-11-04 05:52:42 +0700

    modif percobaan 1

---
+++ modifikasiPercobaan1.java
@@ -0,0 +1,11 @@
+public class modifikasiPercobaan1 {
+    public static void main(String[] args) {
+    
+        int[] bil ={5,13,-7,17};
+
+       for (int i = 0; i<= 3;i++){
+        System.out.println(bil[i]);
+       }
+}
+    
+}
